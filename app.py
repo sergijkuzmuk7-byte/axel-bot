5cab658f7a76c770a6339e74e8f4296daf1c7846
sergijkuzmuk7-byte <sergijkuzmuk7@gmail.com>
2026-05-01 23:38:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+print("STARTING...")
+
 TOKEN = os.getenv("TOKEN")
+
+if TOKEN is None:
+    print("❌ TOKEN IS NONE")
+    exit()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🔥 Бот працює!")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+
+print("✅ BOT STARTED")
 
 app.run_polling()
