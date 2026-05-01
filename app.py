dcd1206b2284f413f948f3cbdb7cbb6c879300de
sergijkuzmuk7-byte <sergijkuzmuk7@gmail.com>
2026-05-01 22:40:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
@@ -7,7 +8,10 @@ TOKEN = os.getenv("TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🔥 Бот працює!")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    await app.run_polling()
 
-app.run_polling()
+if __name__ == "__main__":
+    asyncio.run(main())
